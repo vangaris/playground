1025e975ac8543aaee72cffa06a1996d95090d5b
arislazaridis <arislazaridis@yahoo.com>
2021-11-16 22:28:05 +0200

    confiq mongo

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose')
 
 const username = process.env.MONGO_DB_USERNAME
 const password = process.env.MONGO_DB_PASSWORD
-S
-mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.5wirq.mongodb.net/getOrganized?retryWrites=true&w=majority`, {
+const name = process.env.MONGO_DB_PROJECT_NAME
+mongoose.connect(
+  `mongodb+srv://${username}:${password}@cluster0.5wirq.mongodb.net/${name}?retryWrites=true&w=majority`,
+  {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true
-})
+    useUnifiedTopology: true,
+  },
+)
